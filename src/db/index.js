2b9b2fd755ed0c32e2f913aad1d785cdba479586
assiss <ashish@gmail.com>
2024-06-26 14:19:40 +0530

    daba base connetion

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async ()=>{
+    try{
+        const connectoionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+        console.log(`\n MongoDB connected !! DB HOST: ${connectoionInstance.connection.host}`);
+
+    }catch(error){
+        console.log("MONGODB connection failed", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
